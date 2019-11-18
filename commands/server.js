ce81b19af91f275650da87711b2be347a8acee45
Fabrice Geib <fabricegeib@gmail.com>
2019-11-18 02:57:52 +0100

    Dynamically executing commands

--- commands/server.js
+++ commands/server.js
@@ -1,6 +1,6 @@
 module.exports = {
-	name: 'ping',
-	description: 'Ping!',
+	name: 'server',
+	description: 'Server',
 	execute(message, args) {
         message.channel.send(`This server's name is: ${message.guild.name}`);
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
--- index.js
+++ index.js
@@ -27,13 +27,14 @@ client.on('message', message => {
   const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-  if (command === 'ping') {
-		client.commands.get('ping').execute(message, args);
-	} else if (command === 'beep') {
-		client.commands.get('beep').execute(message, args);
-	} else if (command === 'server') {
-		client.commands.get('server').execute(message, args);
-	}
+  if (!client.commands.has(command)) return;
+
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('there was an error trying to execute that command!');
+  }
 });
 
 
