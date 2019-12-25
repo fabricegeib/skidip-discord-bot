7ffd4cb66475e2f68c6a830ea42f55c390e35c07
Fabrice Geib <fabricegeib@gmail.com>
2019-12-26 00:34:26 +0100

    start add roles

--- index.js
+++ index.js
@@ -30,6 +30,16 @@ client.on('message', message => {
   if (!client.commands.has(commandName)) return;
 
   const command = client.commands.get(commandName);
+
+  if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
   
   try {
     command.execute(message, args);
